3cfdd4846e6a753643fa1e4cb2f94e0d6e311b40
FlyingCat2703 <hanlyngoc1204@gmail.com>
2026-02-22 15:25:41 +0700

    Update Program.cs

--- PSMusic/PSMusic.Server/Program.cs
+++ PSMusic/PSMusic.Server/Program.cs
@@ -126,7 +126,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseOutputCache();
