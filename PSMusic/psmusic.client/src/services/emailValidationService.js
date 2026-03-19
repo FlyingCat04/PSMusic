9316da140da8d48ffe0383bfd49a6e7f37c81dd4
FlyingCat2703 <hanlyngoc1204@gmail.com>
2026-03-19 15:23:23 +0700

    Update emailValidationService.js

--- PSMusic/psmusic.client/src/services/emailValidationService.js
+++ PSMusic/psmusic.client/src/services/emailValidationService.js
@@ -3,18 +3,22 @@ const emailValidationService = {
         try {
             const apiKey = import.meta.env.VITE_ABSTRACT_API_KEY;
             const response = await fetch(
-                `https:///emailreputation.abstractapi.com/v1/?api_key=${apiKey}&email=${email}`
+                `https://emailreputation.abstractapi.com/v1/?api_key=${apiKey}&email=${email}`
             );
             
             const data = await response.json();
             return {
-                isDeliverable: data.email_deliverability?.status === "deliverable" || false,
+                isDeliverable:
+                    data.email_deliverability?.status === "deliverable" &&
+                    data.email_deliverability?.status_detail !== "invalid_mailbox",
                 isFormatValid: data.email_deliverability?.is_format_valid || false,
                 isSMTPValid: data.email_deliverability?.is_smtp_valid || false,
                 isGmail: data.email_sender?.organization_name === "Gmail" || false,
-                message: data.email_deliverability?.status === "deliverable"
-                    ? "Email hợp lệ" 
-                    : "Email không tồn tại hoặc không thể gửi được"
+                message:
+                    data.email_deliverability?.status === "deliverable" &&
+                    data.email_deliverability?.status_detail !== "invalid_mailbox"
+                        ? "Email hợp lệ" 
+                        : "Email không tồn tại hoặc không thể gửi được"
             };
         } catch (error) {
             //console.error("Email validation error:", error);
